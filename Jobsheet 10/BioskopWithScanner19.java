9811cd21645001edf4f5837c360abeca667a2ba4
MeisyNadiaNababan <meisynadianababan@gmail.com>
2023-10-31 15:16:08 +0700

    commit

--- Jobsheet 10/BioskopWithScanner19.java
+++ Jobsheet 10/BioskopWithScanner19.java
@@ -1,29 +1,47 @@
 import java.util.Scanner;
 
-public class BioskopWithScanner19{
-    public static void main(String[] args){
-    Scanner sc= new Scanner(System.in);
-    String[][] penonton = new String[4][2];
-    String nama, next;
-    int baris, kolom;
+public class BioskopWithScanner19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[][] penonton = new String[4][2];
+        String nama, next;
+        int baris, kolom;
 
-    while (true){
-        System.out.print("Masukkan nama: ");
-        nama = sc.nextLine();
-        System.out.print("Masukkan baris: ");
-        baris= sc.nextInt();
-        System.out.print("Masukkan kolom: ");
-        kolom= sc.nextInt();
-        sc.nextLine();
+        while (true) {
+            System.out.println("Menu:");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu: ");
+            int menu = sc.nextInt();
+            sc.nextLine();
 
-        penonton[baris-1][kolom-1] = nama;
-        System.out.print("Input penonton lainnya? (y/n): ");
-        next = sc.nextLine();
+            switch (menu) {
+                case 1:
+                    System.out.print("Masukkan nama: ");
+                    nama = sc.nextLine();
+                    System.out.print("Masukkan baris: ");
+                    baris = sc.nextInt();
+                    System.out.print("Masukkan kolom: ");
+                    kolom = sc.nextInt();
+                    sc.nextLine();
 
-        if (next.equalsIgnoreCase("n")){
-            break;
+                    penonton[baris - 1][kolom - 1] = nama;
+                    break;
+                case 2:
+                    for (int i = 0; i < penonton.length; i++) {
+                        for (int j = 0; j < penonton[i].length; j++) {
+                            if (penonton[i][j] != null) {
+                                System.out.println("Penonton di baris " + (i + 1) + ", kolom " + (j + 1) + ": " + penonton[i][j]);
+                            }
+                        }
+                    }
+                    break;
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("Menu tidak valid. Silakan coba lagi.");
+            }
         }
     }
-    }
-
 }
