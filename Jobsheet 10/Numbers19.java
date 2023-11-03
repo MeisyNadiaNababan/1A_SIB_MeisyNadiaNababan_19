dbeb3893cade15800817dba3702280cb71a776e5
MeisyNadiaNababan <meisynadianababan@gmail.com>
2023-11-03 18:19:32 +0700

    commit

--- Jobsheet 10/Numbers19.java
+++ Jobsheet 10/Numbers19.java
@@ -7,11 +7,11 @@ public class Numbers19{
     myNumbers[1] = new int [3];
     myNumbers[2] = new int [1];
 
-    for (int i = 0; i < myNumbers.length; i++) {
+       for (int i = 0; i < myNumbers.length; i++) {
         System.out.println(Arrays.toString(myNumbers[i]));
     }
     for(int i = 0; i < myNumbers.length; i ++){
         System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
+        }
     }
-}
 }
