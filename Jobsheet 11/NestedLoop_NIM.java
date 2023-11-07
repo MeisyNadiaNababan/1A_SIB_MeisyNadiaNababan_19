75dc6ceed3243712bf2f22793b8834dc34077ede
MeisyNadiaNababan <meisynadianababan@gmail.com>
2023-11-07 13:36:50 +0700

    update

--- Jobsheet 11/NestedLoop_NIM.java
+++ Jobsheet 11/NestedLoop_NIM.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
-public class NestedLoop_NIM{
-    public static void main(String[] args){
+
+public class NestedLoop_NIM {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int [][] temps = new int [5][7];
-        
-        for (int i = 0; i<temps.length; i++){
+        int[][] temps = new int[5][7];
+
+        for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota ke-" + i);
-            for (int j = 0; j<temps[0].length; j++) {
-                System.out.print("Hari ke-" + (j+1) + ": ");
-                temps[i][j]= scanner.nextInt();
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print("Hari ke-" + (j + 1) + ": ");
+                temps[i][j] = scanner.nextInt();
             }
             System.out.println();
         }
-        for (int i=0; i<temps.length; i++){
-            System.out.print("Kota ke-" + (i+1) + ": ");
-            for (int j = 0; j<temps[0].length; j++){
-                System.out.print(temps[i][j] + " ");
+
+        // Display the array using foreach loops
+        int kotaIndex = 1;
+        for (int[] kotaTemps : temps) {
+            System.out.print("Kota ke-" + kotaIndex + ": ");
+            for (int temp : kotaTemps) {
+                System.out.print(temp + " ");
             }
             System.out.println();
+            kotaIndex++;
         }
     }
-
 }
