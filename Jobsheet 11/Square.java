79b0360562afdc8cdaa240a638905795cc2c57ff
MeisyNadiaNababan <meisynadianababan@gmail.com>
2023-11-07 11:05:00 +0700

    update

--- Jobsheet 11/Square.java
+++ Jobsheet 11/Square.java
@@ -4,8 +4,12 @@ public class Square{
         Scanner sc = new Scanner(System.in);
         System.out.print("Maskkan nilai N = ");
         int N = sc.nextInt();
-        for(int i=1 ;i<=N; i++){
-            System.out.print("*");
+
+        for(int iOuter=1; iOuter<=N; iOuter++){
+            for(int i=1; i<=N; i++){
+                            System.out.print("*");
+        } 
+        System.out.println();
         }
     }
 }
