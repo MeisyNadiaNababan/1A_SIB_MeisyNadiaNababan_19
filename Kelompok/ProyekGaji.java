e558c394e4f87c674bde4de3704a87bc52e50039
MeisyNadiaNababan <meisynadianababan@gmail.com>
2023-11-14 09:23:36 +0700

    commit

--- Kelompok/ProyekGaji.java
+++ Kelompok/ProyekGaji.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
-public class ProyekAkhirGajiGuru {
+public class ProyekGaji {
+    static void beriSalam() {
+        System.out.println("Selamat Datang Di Sistem Penggajian Guru");
+        System.out.println("                                        ");
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -12,6 +19,9 @@ public class ProyekAkhirGajiGuru {
         double[] pajakGuru = new double[5];
         int[] jumlahTidakMasukGuru = new int[5];
         double[] gajiBersihGuru = new double[5]; // New array for gaji bersih
+
+        System.out.print("Masukkan sebagai siapa : ");
+        String answer = sc.nextLine();
 
         System.out.println("-----------------MENU-----------------");
         System.out.println("1. Cek Gaji Guru");
@@ -93,15 +103,104 @@ public class ProyekAkhirGajiGuru {
                 break;
             case 2:
                 // Code for handling cek data guru
+                List<String> namaTeacher = new ArrayList<>(); // Jobsheet 9
+                namaTeacher.add("Devin");
+                namaTeacher.add("Meisy");
+                namaTeacher.add("Rahmalia");
+                namaTeacher.add("Belqis");
+                namaTeacher.add("Aqila");
+                List<String> mataPelajaran = new ArrayList<>();
+                mataPelajaran.add("Bahasa Inggris");
+                mataPelajaran.add("Akuntansi");
+                mataPelajaran.add("Seni Budaya");
+                mataPelajaran.add("Matematika");
+                mataPelajaran.add("Bahasa Indonesia");
+                List<Integer> kodeGuru = new ArrayList<>();
+                kodeGuru.add(20040417);
+                kodeGuru.add(20040607);
+                kodeGuru.add(20050528);
+                kodeGuru.add(20040415);
+                kodeGuru.add(20040414);
+                for (int j = 0; j < namaTeacher.size(); j++) {
+                    System.out.println(
+                            "nama guru: " + namaTeacher.get(j) + ", mata pelajaran: " + mataPelajaran.get(j) + ", kode guru: " + kodeGuru.get(j));
+                }
                 break;
             case 3:
                 // Code for handling kehadiran
+                
                 break;
             case 4:
                 // Code for handling slip gaji
+                for (int k = 0; k < 5; k++) {
+                    System.out.println("Guru ke-" + (k + 1) + " - " + namaGuru[k]);
+                    System.out.print("Masukkan NIP Guru: ");
+                    nipGuru[k] = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Jabatan (PNS/HONORER): ");
+                    jabatanGuru[k] = sc.nextLine();
+                    System.out.print("Golongan (1/2): ");
+                    golonganGuru[k] = sc.nextInt();
+                    System.out.print("Gaji pokok: ");
+                    gajiPokokGuru[k] = sc.nextInt();
+
+                    // Calculate pajak based on jabatan and golongan
+                    if (jabatanGuru[k].equalsIgnoreCase("PNS")) {
+                        if (golonganGuru[k] == 1) {
+                            pajakGuru[k] = 0.05;
+                            System.out.println("Pajak (Golongan 1): " + (pajakGuru[k] * 100) + "%");
+                        } else if (golonganGuru[k] == 2) {
+                            pajakGuru[k] = 0.015;
+                            System.out.println("Pajak (Golongan 2): " + (pajakGuru[k] * 100) + "%");
+                        } else {
+                            System.out.println("Golongan PNS yang anda masukkan salah.");
+                            break;
+                        }
+                    } else if (jabatanGuru[k].equalsIgnoreCase("HONORER")) {
+                        if (golonganGuru[k] == 1) {
+                            pajakGuru[k] = 0.02;
+                            System.out.println("Pajak (Golongan 1): " + (pajakGuru[k] * 100) + "%");
+                        } else if (golonganGuru[k] == 2) {
+                            pajakGuru[k] = 0.015;
+                            System.out.println("Pajak (Golongan 2): " + (pajakGuru[k] * 100) + "%");
+                        } else {
+                            System.out.println("Golongan honorer yang anda masukkan salah.");
+                            break;
+                        }
+                    } else {
+                        System.out.println("Jabatan yang anda masukkan salah");
+                        break;
+                    }
+
+                    System.out.print("Masukkan jumlah tidak hadir: ");
+                    jumlahTidakMasukGuru[k] = sc.nextInt();
+                    double potGaji = jumlahTidakMasukGuru[k] * 14400.0;
+
+                    // Calculate gaji bersih
+                    double gajiClean = gajiPokokGuru[k] - (gajiPokokGuru[k] * pajakGuru[k]) - potonganGaji;
+                    gajiBersihGuru[k] = gajiBersih;
+
+                    System.out.println("Total potongan tidak hadir: " + potonganGaji);
+                    System.out.println("Total gaji bersih: " + gajiBersih);
+                    System.out.println();
+                }
+
+                System.out.println("---------- SLIP GAJI GURU ----------");
+                for (int p = 0; p < 5; p++) {
+                    System.out.println("Guru: " + namaGuru[p]);
+                    System.out.println("NIP: " + nipGuru[p]);
+                    System.out.println("Jabatan: " + jabatanGuru[p]);
+                    System.out.println("Golongan: " + golonganGuru[p]);
+                    System.out.println("Gaji Pokok: " + gajiPokokGuru[p]);
+                    System.out.println("Pajak: " + (pajakGuru[p] * 100) + "%");
+                    System.out.println("Potongan Tidak Hadir: " + jumlahTidakMasukGuru[p] * 14400.0);
+                    System.out.println("Gaji Bersih: " + gajiBersihGuru[p]);
+                }
                 break;
             case 5:
                 // Exit the program
+                System.out.println("Terima kasih telah menggunakan layanan ini.");
                 break;
             default:
                 System.out.println("Pilihan tidak valid.");
