fd0ed1790beb85a3368eead33652e7497706dc9d
MeisyNadiaNababan <144025861+MeisyNadiaNababan@users.noreply.github.com>
2023-11-21 12:16:23 +0700

    Add files via upload

---
+++ Terimakasih_18.java
@@ -0,0 +1,10 @@
+public class Terimakasih_18{
+    public static void UcapanTerimakasih(){
+        System.out.println("Thank oyu being the best teacher in the world.\n"+
+        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+
+    public static void main(String[] args){
+        UcapanTerimakasih();
+    }
+}
