0fe1781e90f86c9a8b39f1c2c082c792952ef589
MeisyNadiaNababan <144025861+MeisyNadiaNababan@users.noreply.github.com>
2023-11-21 12:17:27 +0700

    Update Terimakasih_18.java

--- Terimakasih_18.java
+++ Terimakasih_18.java
@@ -1,10 +1,7 @@
 public class Terimakasih_18{
-    public static void UcapanTerimakasih(){
+    public static void main(String[] args){ 
         System.out.println("Thank oyu being the best teacher in the world.\n"+
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
-
-    public static void main(String[] args){
-        UcapanTerimakasih();
     }
 }
