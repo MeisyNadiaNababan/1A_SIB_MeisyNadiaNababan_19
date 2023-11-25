afcd72319cb59d4bf1826d91af26512ffc124222
MeisyNadiaNababan <meisynadianababan@gmail.com>
2023-11-25 07:02:58 +0700

    update

--- Jobsheet 13/Kubus18.java
+++ Jobsheet 13/Kubus18.java
@@ -1,5 +1,26 @@
 import java.util.Scanner;
 
 public class Kubus18{
-    
+    public static int hitungVolume(int sisi){
+        return sisi * sisi * sisi;
+    }
+
+    public static int hitungLuasPermukaan(int sisi){
+        return 6 * sisi * sisi;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan panjang sisi kubus: ");
+        int sisiKubus = sc.nextInt();
+
+        int volume = hitungVolume(sisiKubus);
+        int luasPermukaan = hitungLuasPermukaan(sisiKubus);
+
+        System.out.println("Volume Kubus: " + volume);
+        System.out.println("Luas Permukaan Kubus: " + luasPermukaan);
+
+        sc.close();
+    }
 }
