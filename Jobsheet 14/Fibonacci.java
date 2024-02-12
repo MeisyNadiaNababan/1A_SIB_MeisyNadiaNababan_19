b7081578308071154a66cf4a7981271c03e2a2b2
MeisyNadiaNababan <meisynadianababan@gmail.com>
2024-02-12 13:38:51 +0700

    commit

--- Jobsheet 14/Fibonacci.java
+++ Jobsheet 14/Fibonacci.java
@@ -4,7 +4,7 @@ public class Fibonacci {
 
     static int fibonacci(int n) {
         if (n <= 1) {
-            return n;
+            return n;s
         } else {
             return fibonacci(n - 1) + fibonacci(n - 2);
         }
